090765790e2313824a26f59eaa191fe4958a64d7
davsailor <10620761@polimi.it>
2021-10-21 02:21:19 +0200

    Fixed pretty much everything except for Cassandra that continuously goes in OOMK. Missing MLlib

--- app/app.js
+++ app/app.js
@@ -17,21 +17,39 @@ app.use(express.static("./static"));
 const server = http.createServer(app);
 const socketServer = new WebSocketServer({server: server});
 
+/* define what the server has to do on connection of new clients */
+socketServer.on("request", function(request) {
+	/* accept the request */
+	const connection = request.accept(null, request.origin);
+	
+	/* say hello */
+	connection.on("message", function(message) {
+		console.log("Received message from client: ", message.data);
+		connection.sendUTF("Nice to meet you!");
+	});
+	
+	/* close the connection when client disconnects */
+	connection.on("close", function(reasonCode, decription) {
+		console.log("Client has disconnected");
+	});
+});
+
 /* create and setup the kafka client */
 const kafkaClient = new kafkaNode.KafkaClient({
-										kafkaHost: KAFKA_HOST,
-										connectTimeout: 10000,
-										requestTimeout: 10000,
-										autoConnect: true});
+	kafkaHost: KAFKA_HOST,
+	connectTimeout: 10000,
+	requestTimeout: 10000,
+	autoConnect: true
+});
 
 kafkaClient.on("connect", (error) => {
-										if(error) {
-											console.log("cannot establish connection");
-											throw error;
-										} else {
-											console.log("kafka client is now connected");
-										}
-									});
+	if(error) {
+		console.log("cannot establish connection");
+		throw error;
+	} else {
+		console.log("kafka client is now connected");
+	}
+});
 
 kafkaClient.on("error", (error) => {throw error;});
 
@@ -40,37 +58,38 @@ kafkaClient.on("error", (error) => {throw error;});
 ** reference at: https://www.npmjs.com/package/kafka-node#highlevelproducer-with-keyedpartitioner-errors-on-first-send
 */
 kafkaClient.refreshMetadata([KAFKA_TOPIC], (error) => { 
-														if(error) {
-															console.log(error);
-															throw error;
-														} else {
-															console.log("successfully refreshed metadata");
-														}
-													  });
+	if(error) {
+		console.log(error);
+		throw error;
+	} else {
+		console.log("successfully refreshed metadata");
+	}
+});
 
 /* create and setup the kafka consumer */
 const kafkaConsumer = new kafkaNode.Consumer(kafkaClient, [{topic: KAFKA_TOPIC, partition: 0, offset: 0}], {autoCommit: false});
 const kafkaOffset = new kafkaNode.Offset(kafkaClient);
 
-kafkaConsumer.on("message", (message) => { 
-										socketServer.clients.forEach(client => client.send(message));
-										Log.insert({message})
-											.then(() => {
-															kafkaConsumer.commit((error, data) => console.log(error, data));
-														})
-									});
+kafkaConsumer.on("message", (message) => {
+	console.log(message);
+	console.log(socketServer.clients);
+	if(socketServer.clients.size > 0) {
+		socketServer.clients.forEach((client) => { client.send(message.value); });
+		console.log("Messages sent");
+	}
+});
 
 kafkaConsumer.on("error", (error) => {
-									console.log(error);
-									throw error;
-								});
+	console.log(error);
+	throw error;
+});
 								
 kafkaConsumer.on("offsetOutOfRange", (topic) => {
-												kafkaOffset.fetchLatestOffsets([topic], (error, offsets) => {
-																												const latestOffset = offsets[topic][0];
-																												kafkaConsumer.setOffset(topic, 0, latestOffset);
-																											})
-										   });
+	kafkaOffset.fetchLatestOffsets([topic], (error, offsets) => {
+		const latestOffset = offsets[topic][0];
+		kafkaConsumer.setOffset(topic, 0, latestOffset);
+	})
+});
 
 /* start the server */
 server.listen(PORT);
---
+++ sparkstreaming/consumer.scala
@@ -0,0 +1,175 @@
+/*
+** KAFKA CONSUMER WITH SPARK STREAMING AND MLLIB
+** This file contains the code related to the core of the app: receives data, parses data, compute forecasts
+*/
+
+/* define package */
+package sparkstreaming
+
+/* define imports */
+import scala.util.parsing.json._
+import java.util.HashMap
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka._
+import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import java.util.{Date, Properties}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+import scala.util.Random
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
+import com.datastax.spark.connector.streaming._
+
+
+/* define the case class of the parsed data */
+case class Record(name: String, ts: Double, lon: Double, lat: Double, temp: Double, tempfelt: Double, pressure: Double, humidity: Double, weather: String)
+case class Result(lon: Double, lat: Double, cweather: String, fweather: String, ctemp: Double, ftemp: Double)
+
+/* define class and objects to parse the data coming from the API call */
+class PARSER[T] { def unapply(a: Any): Option[T] = Some(a.asInstanceOf[T]) }
+object MAP extends PARSER[Map[String, Any]]
+object STRING extends PARSER[String]
+object DOUBLE extends PARSER[Double]
+
+
+object ScalaWeatherForecast extends App {
+	
+	/* define constants */
+	val BROKER_URL = "localhost:9092"
+	val RECEIVER_TOPIC = "weather"
+	val SENDER_TOPIC = "forecast"
+
+	/* change log properties */
+	Logger.getLogger("org").setLevel(Level.OFF)
+	
+	/* function to parse data */
+	def parser(string: String): Record = {
+		val newmap = string.substring(1, string.length - 1)
+				.split(",")
+				.map(_.split(":"))
+				.map{case Array(k, v) => (k.substring(1, k.length-1), v)}
+				.toMap
+		Record(newmap("name"), newmap("ts").toDouble, newmap("lon").toDouble, newmap("lat").toDouble, newmap("temp").toDouble, newmap("tempfelt").toDouble, newmap("pressure").toDouble, newmap("humidity").toDouble, newmap("weather"))
+	
+	}
+	
+	/* function to initialize spark session with hdfs */
+	def initSession(): SparkSession = {
+		val ss = SparkSession.builder()
+					.appName("WeatherForecast")
+					.config("spark.cassandra.connection.host", "cassandra")
+		            .config("spark.cassandra.connection.port", "9042")
+					.master("local[2]")
+					.getOrCreate()
+		import ss.implicits._
+		ss
+	}
+	
+	/* function to initialize spark context */
+	def initContext(session: SparkSession): StreamingContext = {
+		val ssc = new StreamingContext(session.sparkContext, Seconds(3))
+		ssc.checkpoint("./tmp/checkpoint")
+		ssc
+	}
+	
+	/* function to initialize kafka receiver */
+	def initKafkaReceiver(context: StreamingContext): InputDStream[(String, String)] = {
+		val conf = Map(
+			"metadata.broker.list" -> "localhost:9092",
+			"zookeeper.connect" -> "localhost:2181",
+			"group.id" -> "kafka-spark-streaming",
+			"zookeeper.connection.timeout.ms" -> "5000")
+		val topics = Set(RECEIVER_TOPIC)
+		val input = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](context, conf, topics)
+		input
+	}
+	
+	/* function to initialize kafka producer */
+	def initKafkaProducer(brokers: String): KafkaProducer[String, String] = {
+		val props = new Properties()
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "ScalaForecastProducer")
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+		new KafkaProducer[String, String](props)
+	}
+	
+	/*
+	** function to build a message with the relevant data for
+	** the app.js client in json format, to simplify the parsing
+	*/
+	def buildMessage(data: Result): String = {
+		val message = """{"lon":""".stripMargin + data.lon.toString +
+					  ""","lat":""".stripMargin + data.lat.toString +
+					  ""","cweather":""".stripMargin + "\"" + data.cweather.toString + "\"" +
+					  ""","fweather":""".stripMargin + "\"" + data.fweather.toString + "\"" +
+					  ""","ctemp":""".stripMargin + data.ctemp.toString +
+					  ""","ftemp":""".stripMargin + data.ftemp.toString + "}"
+		message
+	}
+	
+	/* function to initialize cassandra */
+	def initCassandra(): Session = {
+		val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+		val cs = cluster.connect()
+		cs.execute("""CREATE KEYSPACE IF NOT EXISTS "weather" WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };""".stripMargin)
+		cs.execute("""CREATE TABLE IF NOT EXISTS weather.relevations (name string, ts double, lon double, lat double, temp double, tempfelt double, pressure double, humidity double, weather string, PRIMARY KEY((name, ts)));""".stripMargin)
+		cs
+	}
+	
+	/* function to save the RDD to cassandra */
+	def storeToCassandra(rdd: RDD[(String, String)]): Unit = {
+		val records = rdd.map(w => parser(w._2))
+						 .map(r => Record(r.name, r.ts, r.lon, r.lat, r.temp, r.tempfelt, r.pressure, r.humidity, r.weather));
+		records.saveToCassandra("relevations_keyspace", "weather_table", SomeColumns("name", "ts", "lon", "lat", "temp", "tempfelt", "pressure", "humidity", "weather"))
+	}
+	
+	/* TODO:function to predict stuff
+	** IT HAS TO BE CHANGED, THIS IS JUST TO TEST VISUALIZATION
+	*/
+	def predict(input: Record): Result = {
+		val output = Result(input.lon, input.lat, input.weather, "Clear", input.temp, input.temp+10.0)
+		output
+	}
+	
+	/* define main function */
+	def main() {
+		
+		val ss = initSession()
+		val ssc = initContext(ss)
+		val cs = initCassandra()
+		
+		val input = initKafkaReceiver(ssc)
+
+		input.foreachRDD(rdd => {
+			storeToCassandra(rdd)
+			rdd.foreach(data => {
+				val producer = initKafkaProducer(BROKER_URL)
+				val parsedData = parser(data._2)
+				val result = predict(parsedData)
+				val message = new ProducerRecord[String, String](SENDER_TOPIC, null, buildMessage(result))
+				print(message + "\n")
+				producer.send(message)
+				producer.close()
+			})
+		})
+
+		ssc.start()
+		ssc.awaitTermination()
+		cs.close() 
+	}
+	
+	/* calling the main function */
+	main()
+}
